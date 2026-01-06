30e0873bb5c50874ed50f9e417f15563221a5579
lovekush <lovekush.ug22@nsut.ac.in>
2026-01-07 01:12:16 +0530

    fetch exchange rate api IND <-> USA

--- src/hooks/ExchangeRate.jsx
+++ src/hooks/ExchangeRate.jsx
@@ -8,9 +8,9 @@ export const useExchangeRate = () => {
 
   const fetchRate = async () => {
     const res = await fetch("https://v6.exchangerate-api.com/v6/5d50cd106e2b669f22a61c4e/latest/USD");
-    console.log("rate fetched",res.json());
     const data = await res.json();
-    setRate(data.rates.INR);
+    console.log("rate fetched",data);
+    setRate(data.conversion_rates.INR);
     setLastUpdated(new Date().toLocaleString());
   };
 
